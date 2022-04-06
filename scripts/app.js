b593a31cb94ca35490b46e98cafd5cb7936029de
Pavel Mezencev <mezencev24@mail.ru>
2022-04-06 23:35:52 +0300

    store players names in local storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 let editedPlayer = 0
 
-const players = [
-  {
-    name: '',
-    symbol: 'X',
-  },
-  {
-    name: '',
-    symbol: 'O',
-  },
+const players = JSON.parse(localStorage.getItem('players')) || [
+    {
+        name: '',
+        symbol: 'X',
+    },
+    {
+        name: '',
+        symbol: 'O',
+    },
 ]
 
 const backdrop = document.getElementById('backdrop')
@@ -20,6 +20,8 @@ const nameInputElement = formElement.querySelector('input[name="playername"]')
 const editPlayer1Btn = document.getElementById('edit-player-1-btn')
 const editPlayer2Btn = document.getElementById('edit-player-2-btn')
 const closeEditBtn = document.getElementById('close-edit-btn')
+
+window.addEventListener('load', loadPlayersNames)
 
 editPlayer1Btn.addEventListener('click', openEdit)
 editPlayer2Btn.addEventListener('click', openEdit)
--- scripts/config.js
+++ scripts/config.js
@@ -1,50 +1,67 @@
+function loadPlayersNames() {
+    const players = JSON.parse(localStorage.getItem('players'))
+
+    if (players && players[0].name) {
+        document
+            .getElementById('player-1-data')
+            .querySelector('h3').textContent = players[0].name
+    }
+    if (players && players[1].name) {
+        document
+            .getElementById('player-2-data')
+            .querySelector('h3').textContent = players[1].name
+    }
+}
+
 function openEdit(e) {
-  modal.classList.remove('hidden')
-  backdrop.classList.remove('hidden')
-  nameInputElement.focus()
-  editedPlayer = e.target.dataset.player
+    modal.classList.remove('hidden')
+    backdrop.classList.remove('hidden')
+    nameInputElement.focus()
+    editedPlayer = e.target.dataset.player
 }
 
 function closeEdit() {
-  modal.classList.add('hidden')
-  backdrop.classList.add('hidden')
-  configErrorMsg.textContent = ''
-  nameInputElement.value = ''
-  formElement.classList.remove('error')
+    modal.classList.add('hidden')
+    backdrop.classList.add('hidden')
+    configErrorMsg.textContent = ''
+    nameInputElement.value = ''
+    formElement.classList.remove('error')
 }
 
 function editPlayerName(e) {
-  e.preventDefault()
+    e.preventDefault()
 
-  const formDataValue = new FormData(e.target).get('playername').trim()
+    const formDataValue = new FormData(e.target).get('playername').trim()
 
-  if (!formDataValue) {
-    configErrorMsg.textContent = 'Please, enter your name!'
-    formElement.classList.add('error')
-    return
-  }
-  if (formDataValue.length < 3) {
-    configErrorMsg.textContent =
-      'Player name must include at least 3 characters!'
-    formElement.classList.add('error')
-    return
-  }
+    if (!formDataValue) {
+        configErrorMsg.textContent = 'Please, enter your name!'
+        formElement.classList.add('error')
+        return
+    }
+    if (formDataValue.length < 3) {
+        configErrorMsg.textContent =
+            'Player name must include at least 3 characters!'
+        formElement.classList.add('error')
+        return
+    }
 
-  players[editedPlayer - 1].name = formDataValue
-  document
-    .getElementById(`player-${editedPlayer}-data`)
-    .querySelector('h3').textContent = formDataValue
+    players[editedPlayer - 1].name = formDataValue
+    localStorage.setItem('players', JSON.stringify(players))
 
-  nameInputElement.value = ''
-  formElement.classList.remove('error')
-  closeEdit()
+    document
+        .getElementById(`player-${editedPlayer}-data`)
+        .querySelector('h3').textContent = formDataValue
+
+    nameInputElement.value = ''
+    formElement.classList.remove('error')
+    closeEdit()
 }
 
 function inputValidate(e) {
-  if (!formElement.classList.contains('error')) return
+    if (!formElement.classList.contains('error')) return
 
-  if (nameInputElement.value.length > 2) {
-    configErrorMsg.textContent = ''
-    formElement.classList.remove('error')
-  }
+    if (nameInputElement.value.length > 2) {
+        configErrorMsg.textContent = ''
+        formElement.classList.remove('error')
+    }
 }
