decb4263f61f96b41a0a9982cd619882dfd443f6
Pavel Mezencev <mezencev24@mail.ru>
2022-04-06 15:50:06 +0300

    add form validation

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,31 @@
+let editedPlayer = 0
+
+const players = [
+  {
+    name: '',
+    symbol: 'X',
+  },
+  {
+    name: '',
+    symbol: 'O',
+  },
+]
+
 const backdrop = document.getElementById('backdrop')
 const modal = document.getElementById('config-overlay')
+const configErrorMsg = document.getElementById('config-error')
+const formElement = document.querySelector('form')
+const nameInputElement = formElement.querySelector('input[name="playername"]')
+
 const editPlayer1Btn = document.getElementById('edit-player-1-btn')
 const editPlayer2Btn = document.getElementById('edit-player-2-btn')
 const closeEditBtn = document.getElementById('close-edit-btn')
 
 editPlayer1Btn.addEventListener('click', openEdit)
 editPlayer2Btn.addEventListener('click', openEdit)
+
 backdrop.addEventListener('click', closeEdit)
 closeEditBtn.addEventListener('click', closeEdit)
+
+formElement.addEventListener('submit', editPlayerName)
+nameInputElement.addEventListener('input', inputValidate)
--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,50 @@
-function openEdit() {
+function openEdit(e) {
   modal.classList.remove('hidden')
   backdrop.classList.remove('hidden')
+  nameInputElement.focus()
+  editedPlayer = e.target.dataset.player
 }
 
 function closeEdit() {
   modal.classList.add('hidden')
   backdrop.classList.add('hidden')
+  configErrorMsg.textContent = ''
+  nameInputElement.value = ''
+  formElement.classList.remove('error')
+}
+
+function editPlayerName(e) {
+  e.preventDefault()
+
+  const formDataValue = new FormData(e.target).get('playername').trim()
+
+  if (!formDataValue) {
+    configErrorMsg.textContent = 'Please, enter your name!'
+    formElement.classList.add('error')
+    return
+  }
+  if (formDataValue.length < 3) {
+    configErrorMsg.textContent =
+      'Player name must include at least 3 characters!'
+    formElement.classList.add('error')
+    return
+  }
+
+  players[editedPlayer - 1].name = formDataValue
+  document
+    .getElementById(`player-${editedPlayer}-data`)
+    .querySelector('h3').textContent = formDataValue
+
+  nameInputElement.value = ''
+  formElement.classList.remove('error')
+  closeEdit()
+}
+
+function inputValidate(e) {
+  if (!formElement.classList.contains('error')) return
+
+  if (nameInputElement.value.length > 2) {
+    configErrorMsg.textContent = ''
+    formElement.classList.remove('error')
+  }
 }
